91a2cbd1afc3e8e460c4af32addfb8fff48156cb
Sky <44381751+skylarr1227@users.noreply.github.com>
2023-06-03 16:43:37 -0400

    formatting

--- supportbot/cogs/tickets.py
+++ supportbot/cogs/tickets.py
@@ -126,13 +126,13 @@ class Tickets(commands.Cog):
                         # Extract the first 20 words from the cleaned content of the message
                         first_20_words = " ".join(cleaned_message.split()[:20])
                         thread_groups[tag].append((thread_name, first_20_words, thread.id))
-        embed = discord.Embed(title="Open Threads", color=discord.Color.blue())
+        embed = discord.Embed(title="Known Issues by Platform", color=discord.Color.red())
         # Construct the description of the embed by iterating over each group of threads
         description = ""
         for tag, threads in thread_groups.items():
             description += f"## {tag} Issues\n"
             for thread_name, first_20_words, thread_id in threads:
-                description += f"**{thread_name}** - {first_20_words}... [continue reading here](https://discord.com/channels/774124295026376755/{channel_id}/{thread_id})\n"
+                description += f"- **{thread_name}** - {first_20_words}... [continue reading here](https://discord.com/channels/774124295026376755/{channel_id}/{thread_id})\n"
             description += "\n"
 
         embed.description = description
