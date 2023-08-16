4c0e3afa99096ac1550690b121eec0be705e3540
DittoBOT3 <skylarr12227@gmail.com>
2023-08-16 12:45:43 -0400

    stuff local

--- supportbot/core/bot.py
+++ supportbot/core/bot.py
@@ -67,7 +67,8 @@ class SupportBot(commands.AutoShardedBot):
             395788963475881985,
             988106635874533467,
         ]
-
+        BITLY_KEY = os.environ.get("BITLY")
+        self.BITLY_KEY = BITLY_KEY
         self.closed_tickets = {
             'RESOLVED': 0,
             'ON-GOING': 0,
@@ -81,23 +82,34 @@ class SupportBot(commands.AutoShardedBot):
         SUPABASE_URL = os.environ.get("SUPABASE_URL") # this is the supabase url
         self.SUPABASE_URL = SUPABASE_URL
         NOTION_TOKEN = os.environ.get("NOTION_TOKEN")
-        OPENAI_KEY = os.environ.get("OPENAI_API_KEY")
+        OPENAI_KEY = os.environ.get("OPENAI_KEY")
         NOTION_DATABASE_URL = os.environ.get("NOTION_DATABASE_URL")
         FRESHDESK_API_KEY = os.environ.get("FRESHDESK_API_KEY")
         FRESHDESK_DOMAIN = os.environ.get("FRESHDESK_DOMAIN")
         self.logger = logging.Logger("supportbot")
         supabase: Client = create_client(SUPABASE_URL, SUPABASE_API_KEY)
         self.supabase = supabase
-        self.notion_client = AsyncClient(auth=NOTION_TOKEN)
-        self.collection = self.notion_client.databases.retrieve("b48e1f0a4f2e4a758992ba1931a35669")
-        self.openai = os.environ.get(OPENAI_KEY)
+        self.NOTION_TOKEN = NOTION_TOKEN
+        #self.collection = self.notion_client.databases.retrieve("b48e1f0a4f2e4a758992ba1931a35669")
+        self.OPENAI_KEY = OPENAI_KEY
         self.SPECIFIC_POST_CHANNEL_ID = 1102722546232729620
-        self.openai = openai.api_key
+        #self.openai = openai.api_key
         self.api_key = FRESHDESK_API_KEY
         self.domain = FRESHDESK_DOMAIN
         self.api_url = f"https://{self.domain}.freshdesk.com/api/v2/"
 
-   
+    async def create_notion_client(self):
+        try:
+            self.notion_client = AsyncClient(auth=self.NOTION_TOKEN)
+            self.collection = self.notion_client.databases.retrieve("b48e1f0a4f2e4a758992ba1931a35669")
+        except Exception as e:
+            await self.on_error("create_notion_client", e)
+        return self.notion_client
+
+
+
+        
+
     async def analyze_sentiment_and_participation(self, thread_id):
         thread = self.get_channel(thread_id)
         if not isinstance(thread, discord.Thread):
@@ -119,7 +131,7 @@ class SupportBot(commands.AutoShardedBot):
 
 
     async def ask_gpt(self, question):
-        openai.api_key = self.openai
+        openai.api_key = self.OPENAI_KEY
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", 
             messages=[
@@ -137,7 +149,7 @@ class SupportBot(commands.AutoShardedBot):
     
 
     async def analyze_sentiment(self, user_messages):
-        openai.api_key = self.openai
+        openai.api_key = self.OPENAI_KEY
         messages = [{"role": "system", "content": "You are a helpful assistant."}]
         for message in user_messages:
             messages.append({"role": "user", "content": f"What is the sentiment of this message: '{message}'?"})
@@ -153,7 +165,7 @@ class SupportBot(commands.AutoShardedBot):
     async def on_ready(self):
         self.logger.info(f'{self.user.name} has connected to Discord!')
         specific_post_channel = self.get_channel(1102722546232729620)
-        
+        await self.create_notion_client()
         if specific_post_channel is None:
             thread = await specific_post_channel.create_thread(
                 name="This is a test",content="Known issues will be listed here with links to each post")   
