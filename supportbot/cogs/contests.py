4e5b9d923ac0554ed6473990fb5d637dd2ba9a73
Sky <44381751+skylarr1227@users.noreply.github.com>
2023-08-16 10:48:16 -0400

    Metrics, announcements, contests, polls

--- supportbot/cogs/contests.py
+++ supportbot/cogs/contests.py
@@ -1,108 +1,112 @@
-import discord
-from discord.ext import commands
-from pytz import timezone
-from datetime import datetime
 import asyncio
-import time
+import calendar
+from datetime import datetime, timedelta
+from pytz import timezone
+from discord.ext import commands
+import discord
 
-INSPECTION_CHANNEL_ID = 1128673991138222151
+THEME_CHANNEL_ID = 
+INSPECTION_CHANNEL_ID = 
 PUBLIC_VOTING_CHANNEL_ID = 
-CHANNEL_ID = 
 
 class Contests(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.accepting_images = True
         self.bot.loop.create_task(self.check_time())
-    
+        self.bot.loop.create_task(self.count_votes())
 
-    async def check_time(self):
-        while True:
-            # Check the current time
-            now = datetime.now(timezone('UTC'))
-            
-            # If it's 9pm UTC (4pm EST), stop accepting images
-            if now.hour == 21:
-                self.accepting_images = False
-            else:
-                self.accepting_images = True
-            
-            # Wait 60 seconds before checking the time again
-            await asyncio.sleep(60)
-    
-    async def inspect_image(self, user_id, image_url):
-        # Send the image to a specific channel for manual inspection
-        channel = self.bot.get_channel(INSPECTION_CHANNEL_ID)  # Replace INSPECTION_CHANNEL_ID with the ID of your inspection channel
-        message = await channel.send(f"New image submission from <@{user_id}> for inspection:", embed=discord.Embed().set_image(url=image_url))
-        
-        # Add the thumbs up and down emojis as reactions
-        await message.add_reaction("👍")
-        await message.add_reaction("👎")
-
-    async def post_image(self, user_id, image_url):
-        # Post the image to a specific channel for public voting
-        channel = self.bot.get_channel(PUBLIC_VOTING_CHANNEL_ID)  # Replace PUBLIC_VOTING_CHANNEL_ID with the ID of your public voting channel
-        message = await channel.send(f"Image submission from <@{user_id}>:", embed=discord.Embed().set_image(url=image_url))
-        
-        # Add the thumbs up emoji as a reaction
-        await message.add_reaction("👍")
-  
     async def get_theme(self):
-        # Query the "contests" table in Supabase to get the theme for the current day
-        # You'll need to adjust this query based on the structure of your "contests" table
-        result = await self.bot.supabase.table("contests").select().execute()
-        return result.data[0]["theme"]
+        now = datetime.now(timezone('UTC'))
+        week_of_year = now.isocalendar()[1]
+        day_of_week = calendar.day_name[now.weekday()].lower()
+        result = await self.bot.supabase.table("contests").select(day_of_week).filter('week', 'eq', week_of_year).execute()
+        return result.data[0][day_of_week]
 
     @commands.Cog.listener()
     async def on_ready(self):
         theme = await self.get_theme()
-        # Send the theme to a specific channel
-        channel = self.bot.get_channel(CHANNEL_ID)  # Replace CHANNEL_ID with the ID of your channel
-        embed = discord.Embed(title="Today's Contest Theme", description=theme)
-        await channel.send(embed=embed)
-    
-    
+        channel = self.bot.get_channel(THEME_CHANNEL_ID)  
+        await channel.send(f"Today's theme is: {theme}")
+
+    async def inspect_image(self, user_id, image_url):
+        channel = self.bot.get_channel(INSPECTION_CHANNEL_ID)  
+        message = await channel.send(f"New image submission from <@{user_id}> for inspection:", embed=discord.Embed().set_image(url=image_url))
+        await message.add_reaction("👍")
+        await message.add_reaction("👎")
+
+    async def post_image(self, user_id, image_url):
+        channel = self.bot.get_channel(PUBLIC_VOTING_CHANNEL_ID) 
+        message = await channel.send(f"Image submission from <@{user_id}>:", embed=discord.Embed().set_image(url=image_url))
+        await message.add_reaction("👍")
+        # Update the user's record in the "users" table in Supabase with the message ID
+        await self.bot.supabase.table('users').update({'message_id': message.id}).match({'u_id': user_id}).execute()
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        # Check if the message is a DM and contains an image
-        if isinstance(message.channel, discord.DMChannel) and message.attachments:
-            # Send a confirmation message
-            confirm_message = await message.channel.send("Do you want to submit this image for the daily contest? Reply with 'yes' to confirm.")
-            
+        if isinstance(message.channel, discord.DMChannel) and len(message.attachments) > 0:
+            reply = await message.reply("Do you want to submit this image for the daily contest? (yes/no)")
             def check(m):
-                # Check that the reply is from the same user and in the same channel
-                return m.author == message.author and m.channel == message.channel
+                return m.author == message.author and m.channel == message.channel and m.content.lower() in ["yes", "no"]
 
             try:
-                # Wait for a reply to the confirmation message
-                reply = await self.bot.wait_for('message', timeout=60.0, check=check)
+                reply = await self.bot.wait_for('message', check=check, timeout=60.0)
             except asyncio.TimeoutError:
-                # If the user doesn't reply within 60 seconds, send a timeout message
-                await message.channel.send('Sorry, you did not reply in time.')
+                await message.author.send('Sorry, you took too long to reply.')
             else:
-                # If the user replies with 'yes', log their ID and the current timestamp in Supabase
                 if reply.content.lower() == 'yes' and self.accepting_images:
-                    await self.bot.supabase.table("users").upsert({"u_id": message.author.id, "submitted": int(time.time())}).execute()
+                    await self.bot.supabase.table('users').upsert({'u_id': message.author.id, 'submitted': int(datetime.now(timezone('UTC')).timestamp())}).execute()
                     await self.inspect_image(message.author.id, message.attachments[0].url)
-                    await message.channel.send('Your image has been submitted for the daily contest.')
-                else:
-                    await message.channel.send('Your image has not been submitted.')
+                    await message.author.send('Your image has been submitted for manual inspection.')
+                elif reply.content.lower() == 'no':
+                    await message.author.send('Okay, your image was not submitted.')
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        # Check if the reaction is in the inspection channel and was added by a staff member
         if payload.channel_id == INSPECTION_CHANNEL_ID and payload.user_id in self.bot.WOMBO_SUPPORT:
-            # Get the message that the reaction was added to
             channel = self.bot.get_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
-            
-            # Check if the reaction is the thumbs up emoji
+
             if str(payload.emoji) == "👍":
-                # If the image passes manual inspection, post it to the public voting channel
-                user_id = message.embeds[0].description.split("<@")[1].split(">")[0]  # Extract the user ID from the message embed
-                image_url = message.embeds[0].image.url  # Get the image URL from the message embed
+                user_id = message.embeds[0].description.split("<@")[1].split(">")[0]
+                image_url = message.embeds[0].image.url
                 await self.post_image(user_id, image_url)
+
+    async def check_time(self):
+        while True:
+            now = datetime.now(timezone('UTC'))
+            if now.hour == 21:
+                self.accepting_images = False
+            else:
+                self.accepting_images = True
+            await asyncio.sleep(60)
+
+    async def count_votes(self):
+        while True:
+            now = datetime.now(timezone('UTC'))
+
+            if now.hour == 22:
+                channel = self.bot.get_channel(PUBLIC_VOTING_CHANNEL_ID)
+                vote_counts = {}
+                result = await self.bot.supabase.table('users').select('u_id, message_id').execute()
+
+                for row in result.data:
+                    message = await channel.fetch_message(row['message_id'])
+                    for reaction in message.reactions:
+                        if str(reaction.emoji) == "👍":
+                            vote_counts[row['u_id']] = reaction.count
+
+                winners = sorted(vote_counts, key=vote_counts.get, reverse=True)[:5]
+                await channel.send(f"The winners are: {', '.join(f'<@{winner}>' for winner in winners)}")
+
+                # Update XP and levels for winners
+                xp_awards = [100, 80, 60, 40, 20]  # XP for 1st to 5th places
+                for i, winner in enumerate(winners):
+                    user = await self.bot.supabase.table('users').select('u_id, xp, level').filter('u_id', 'eq', winner).single().execute()
+                    new_xp = user.data['xp'] + xp_awards[i]
+                    new_level = new_xp // 100  # each level requires 100 XP
+                    await self.bot.supabase.table('users').update({'xp': new_xp, 'level': new_level}).match({'u_id': winner}).execute()
+            await asyncio.sleep(60)
 
 async def setup(bot):
     await bot.add_cog(Contests(bot))
---
+++ supportbot/cogs/poll.py
@@ -0,0 +1,96 @@
+import random
+import discord
+from discord.ext import commands
+from discord import Embed, Colour, app_commands
+from supportbot.core.utils import team, support, store_in_supabase, store_prompt
+
+
+class Polls(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.emoji_numbers = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"]
+
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        # Ignore the bot's own reactions
+        if user == self.bot.user:
+            return
+        response = await self.bot.supabase.table("polls").select("*").eq('message_id', reaction.message.id).execute()
+        if response.error or not response.data:
+            return
+        poll = response.data[0]
+        for embed in reaction.message.embeds:
+            for i, option in enumerate(poll['options']):
+                if str(reaction.emoji) == self.emoji_numbers[i] and option in str(embed.title):
+                    # A vote was added, update the poll
+                    poll['votes'][option] += 1
+                    await self.bot.supabase.table("polls").update({'votes': poll['votes']}).eq('p_id', poll['p_id']).execute()
+                    break
+
+
+    @team()
+    @app_commands.command()
+    async def start_poll(self, interaction, channel: discord.TextChannel, poll_name: str, option1: str, option2: str, poll_question: str, option3: str = None, option4: str = None, option5: str = None):
+        ctx = await self.bot.get_context(interaction)
+        await interaction.response.defer()
+        options = {option1: 0, option2: 0}
+        if option3:
+            options[option3] = 0
+        if option4:
+            options[option4] = 0
+        if option5:
+            options[option5] = 0
+        embed = Embed(title=poll_name, description=poll_question, color=Colour.blue())
+        for i, option in enumerate(options):
+            embed.add_field(name=f"{self.emoji_numbers[i]} {option}", value="React accordingly", inline=False)
+
+        Message = await channel.send(embed=embed)
+        votes = {option: 0 for option in options}
+        data = {
+            'name': poll_name,
+            'poll': poll_question,
+            'started_by': interaction.user.id,
+            'options': options,
+            'votes': options,
+            'message_id': Message.id 
+        }
+
+        response = await self.bot.supabase.table("polls").insert(data).returning("p_id").execute()
+
+        if response.error:
+            await ctx.send(f"Error starting poll: {response.error}")
+        else:
+            poll_id = response.data[0]['p_id']
+            await ctx.send(f"The poll {poll_name}(`{poll_id}`) started successfully in:\n### <#{channel.id}>.")
+        
+
+    @team()
+    @app_commands.command()
+    async def end_poll(self, interaction, poll_id: int):
+        ctx = await self.bot.get_context(interaction)
+        await interaction.response.defer()
+        response = await self.bot.supabase.table("polls").delete().eq('p_id', poll_id).execute()
+        if response.error:
+            await ctx.send(f"Error ending poll: {response.error}")
+        else:
+            await ctx.send(f"Poll {poll_id} ended successfully.")
+
+    @commands.command()
+    async def show_polls(self, ctx):
+        response = await self.bot.supabase.table("polls").select("*").execute()
+
+        if response.error:
+            await ctx.send(f"Error fetching polls: {response.error}")
+        else:
+            for poll in response.data:
+                embed = Embed(title=f"{poll['name']} (ID: {poll['p_id']})", description=poll['poll'], color=Colour.blue())
+                for option, votes in poll['votes'].items():
+                    total_votes = sum(poll['votes'].values())
+                    percent = (votes / total_votes) * 100 if total_votes else 0
+                    bar = "".join(["▰" if i < round(percent / 10) else "▱" for i in range(10)])
+                    embed.add_field(name=option, value=f"{bar} {percent}%", inline=False)
+                await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Polls(bot))
