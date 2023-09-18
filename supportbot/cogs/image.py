b88fccd6e19dd3a9e609036d2314a015ffdfb4bb
Sky <44381751+skylarr1227@users.noreply.github.com>
2023-09-18 12:49:44 -0400

    s

--- supportbot/cogs/image.py
+++ supportbot/cogs/image.py
@@ -15,14 +15,23 @@ class ImageCollage(commands.Cog):
 
     @team()
     @app_commands.command()
-    @app_commands.describe(image_file="Drag your image file here")
-    async def submit(self, interaction, image_file: discord.Attachment):
+    @app_commands.describe(image_file="Drag your image file here", )
+    async def submit(self, interaction, image_file: discord.Attachment, image_file2: discord.Attachment=None, image_file3: discord.Attachment=None, image_file4: discord.Attachment=None):
         """
         Submit an image for the collage.
         """
         image_data = await image_file.read(use_cached=True)
         self.images.append(image_data)
-        await interaction.response.send_message("Image uploaded successfully!")
+        if image_file2:
+            image_data = await image_file2.read(use_cached=True)
+            self.images.append(image_data)
+        if image_file3:
+            image_data = await image_file3.read(use_cached=True)
+            self.images.append(image_data)
+        if image_file4:
+            image_data = await image_file4.read(use_cached=True)
+            self.images.append(image_data)
+        await interaction.response.send_message("Image(s) uploaded successfully!")
     
     @team()
     @app_commands.command()
@@ -30,6 +39,7 @@ class ImageCollage(commands.Cog):
         """
         Generate a collage of all submitted images.
         """
+        await interaction.response.defer()
         # Convert the list of images bytes to PIL images
         if not self.images:
             await interaction.response.send_message("Submitted Successfully")
@@ -43,7 +53,7 @@ class ImageCollage(commands.Cog):
         with io.BytesIO() as image_binary:
             collage_img.save(image_binary, 'PNG')
             image_binary.seek(0)
-            await interaction.response.send_message(file=discord.File(fp=image_binary, filename="collage.png"))
+            await interaction.response.followup.send(file=discord.File(fp=image_binary, filename="collage.png"))
 
     def _create_collage(self, images):
         """
@@ -65,7 +75,7 @@ class ImageCollage(commands.Cog):
         collage_draw = ImageDraw.Draw(collage_img)
 
         # Load a font for numbering
-        font = ImageFont.load_default()
+        font = ImageFont.truetype("futur.ttf", 25)
 
         x_offset, y_offset = 0, 0
         image_counter = 0
@@ -76,7 +86,7 @@ class ImageCollage(commands.Cog):
                     # Paste the image into the collage
                     collage_img.paste(img, (x_offset, y_offset))
                     # Draw the number on the image
-                    collage_draw.text((x_offset + 10, y_offset + 10), str(image_counter + 1), fill=(0, 0, 0, 255), font=font)
+                    collage_draw.text((x_offset + 10, y_offset + 10), str(image_counter + 1), fill=(253, 253, 1, 255), font=font)
                     x_offset += img.width
                     image_counter += 1
             y_offset += img.height
